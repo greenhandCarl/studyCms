965356edd23631fdec1359e536c03e55ef0e5e38
zhengjin <511252519@qq.com>
2020-04-17 22:48:30 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,81 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='form-container'>
+      <el-form ref="form" :model="form" label-width="200px">
+        <el-form-item label="老师名字">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="头像">
+          <el-input v-model="form.avatar"></el-input>
+        </el-form-item>
+        <el-form-item label="视频介绍">
+          <el-input v-model="form.video"></el-input>
+        </el-form-item>
+        <el-form-item label="文字介绍">
+          <el-input v-model="form.introduct"></el-input>
+        </el-form-item>
+        <el-form-item label="学历">
+          <el-input v-model="form.education"></el-input>
+        </el-form-item>
+        <el-form-item label="学院">
+          <el-input v-model="form.college"></el-input>
+        </el-form-item>
+        <el-form-item label="专业">
+          <el-input v-model="form.profession"></el-input>
+        </el-form-item>
+        <el-form-item label="擅长科目">
+          <el-input v-model="form.expert"></el-input>
+        </el-form-item>
+        <el-form-item label="年限">
+          <el-input v-model="form.years"></el-input>
+        </el-form-item>
+        <el-form-item label="是否是教师">
+          <el-radio-group v-model="form.isTeacher">
+            <el-radio label="1">是</el-radio>
+            <el-radio label="0">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="是否支持视频教学">
+          <el-radio-group v-model="form.liveTeachingStatus">
+            <el-radio label="1">是</el-radio>
+            <el-radio label="0">否</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+import axios from 'axios'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+@Component
+export default class Home extends Vue {
+  form = {}
+
+  onSubmit () {
+    console.log('this.form', this.form)
+    axios({
+      method: 'post',
+      url: 'http://120.26.77.52:8080/subject_teacher',
+      data: this.form
+    })
   }
 }
 </script>
+
+<style lang='scss' scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  .form-container {
+    width: 50%;
+  }
+}
+</style>
